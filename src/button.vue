b7602d9cfa079ced6aa284e5297b5a288c31041d
wuxiao <1291383707@qq.com>
2019-11-08 12:05:40 +0800

    加入 icon ，并判定是否需要icon以及icon 的位置在哪

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,24 @@
 <template>
-    <button class="g-button">按钮</button>
+    <div>
+        <button class="g-button" v-if="iconPosition === 'right'">
+            <slot></slot>
+            <svg v-if="icon" class="icon">
+                <use :xlink:href=`#i-${icon}`></use>
+            </svg>
+        </button>
+        <button class="g-button" v-else>
+            <svg v-if="icon" class="icon">
+                <use :xlink:href=`#i-${icon}`></use>
+            </svg>
+            <slot></slot>
+        </button>
+    </div>
+
 </template>
 
 <script>
     export default {
-
+        props: ['icon', 'iconPosition']
     }
 </script>
 
@@ -16,12 +30,15 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+
         &:hover {
-        border-color: var(--border-color-hover);
+            border-color: var(--border-color-hover);
         }
+
         &:active {
             background: var(--button-active-bg);
         }
+
         &:focus {
             outline: none;
         }
