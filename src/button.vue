19b485488e6bb550fa7741dce83e69106161c4d6
wuxiao <1291383707@qq.com>
2019-11-08 13:02:03 +0800

    添加下载、加载中svg

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
         <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-            <g-icon v-if="icon" :name="icon"></g-icon>
+            <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+            <g-icon name="loading" class="loading"></g-icon>
             <div class="content">
                 <slot></slot>
             </div>
@@ -25,6 +26,13 @@
 </script>
 
 <style lang="scss">
+    @keyframes spin {
+        0%{ transform: rotate(0deg);}
+        100%{ transform: rotate(360deg);}
+    }
+    .loading{
+        animation: spin 2s infinite linear;
+    }
     .g-button {font-size: var(--font-size);height: var(--button-height);padding: 0 1em;
         border-radius: var(--border-radius);border: 1px solid var(--border-color);
         background: var(--button-bg);display: inline-flex;justify-content: center;align-items: center;
