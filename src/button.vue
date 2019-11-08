1714ffb32a5e8172ca90f6d2eede337426cacf49
wuxiao <1291383707@qq.com>
2019-11-08 12:30:41 +0800

    优化 icon-position属性

--- src/button.vue
+++ src/button.vue
@@ -1,46 +1,39 @@
 <template>
-    <div>
-        <button class="g-button" v-if="iconPosition === 'right'">
-            <slot></slot>
+        <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
             <svg v-if="icon" class="icon">
                 <use :xlink:href=`#i-${icon}`></use>
             </svg>
+            <div class="content"><slot></slot></div>
+<!--            slot内不能加class-->
         </button>
-        <button class="g-button" v-else>
-            <svg v-if="icon" class="icon">
-                <use :xlink:href=`#i-${icon}`></use>
-            </svg>
-            <slot></slot>
-        </button>
-    </div>
-
 </template>
 
 <script>
     export default {
-        props: ['icon', 'iconPosition']
+        //props: ['icon', 'iconPosition']
+        props:{
+            icon: {},
+            iconPosition:{
+                type: String,
+                default: 'left'
+            }
+        }
     }
 </script>
 
 <style lang="scss">
-    .g-button {
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
-
-        &:hover {
-            border-color: var(--border-color-hover);
-        }
-
-        &:active {
-            background: var(--button-active-bg);
-        }
-
-        &:focus {
-            outline: none;
+    .g-button {font-size: var(--font-size);height: var(--button-height);padding: 0 1em;
+        border-radius: var(--border-radius);border: 1px solid var(--border-color);
+        background: var(--button-bg);display: inline-flex;justify-content: center;align-items: center;
+        vertical-align: middle;//解决对不齐的css问题
+        &:hover {border-color: var(--border-color-hover); }
+        &:active { background: var(--button-active-bg); }
+        &:focus { outline: none; }
+        > .content{ order: 2;}
+        > .icon{ order: 1;margin-right: .1em;margin-left: 0;}
+        &.icon-right{
+            > .content{ order: 1; }
+            > .icon{ order: 2;margin-left: .1em;margin-right: 0; }
         }
     }
 
