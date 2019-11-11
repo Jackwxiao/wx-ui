b9088d957a14d8c1d11d2cb041bf5a04ee183a7f
wuxiao <1291383707@qq.com>
2019-11-11 10:01:12 +0800

    row col 雏形完成

---
+++ src/col.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'GuLuCol',
+        props:{
+            span: [Number,String]
+        }
+    }
+</script>
+<style scoped lang="scss">
+    .col{
+        height: 100px;
+        background: grey;
+        width: 50%;
+        border: 1px solid red;
+        $class-prefix: col-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                width: ($n /24) * 100%;
+            }
+        }
+    }
+
+</style>
---
+++ src/row.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="row">
+        <slot></slot>
+    </div>
+</template>
+<script>
+
+</script>
+<style lang="scss" scoped>
+    .row{display: flex;}
+</style>
