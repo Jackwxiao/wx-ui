134f63f08c79532d84f7e3531cac011a8c974a3e
wuxiao <1291383707@qq.com>
2019-11-13 14:46:21 +0800

    实现 自动关闭和点击关闭 功能

--- src/plugin.js
+++ src/plugin.js
@@ -5,7 +5,16 @@ export default {
         Vue.prototype.$toast = function (message) {
             //动态创建实例（组件）
             let Constructor = Vue.extend(Toast)
-            let toast = new Constructor()
+            let toast = new Constructor({
+                propsData:{
+                    closeButton: {
+                        text: '知道了',
+                        callback(){
+                            console.log('用户说ta知道了')
+                        }
+                    }
+                }
+            })
             toast.$slots.default = [message]
             toast.$mount()
             document.body.appendChild(toast.$el)
--- src/toast.vue
+++ src/toast.vue
@@ -1,23 +1,64 @@
 <template>
     <div class="toast">
         <slot></slot>
+        <div class="line"></div>
+        <span v-if="closeButton" class="close" @click="onClickClose">{{closeButton.text}}</span>
     </div>
 </template>
 <script>
 export default {
     name: 'GuLuToast',
-
+    props:{
+        autoClose:{
+            type:Boolean,
+            default: true
+        },
+        autoCloseDelay:{
+            type: Number,
+            default: 5
+        },
+        closeButton:{
+            type: Object,
+            default(){
+                return {
+                    text: '关闭',callback: (toast) => {
+                        toast.close()
+                    }
+                }
+            }
+        }
+    },
+    mounted() {
+        if (this.autoClose){
+            setTimeout(()=>{
+                this.close()
+            },this.autoCloseDelay*1000)
+        }
+    },
+    methods:{
+        close(){
+            this.$el.remove()
+            this.$destroy()
+        },
+        onClickClose(){
+            this.close()
+            if (this.closeButton && typeof this.closeButton.callback ==='function'){
+                this.closeButton.callback()
+            }
+        }
+    },
 }
 </script>
 <style scoped lang="scss">
     $font-size: 14px;
     $toast-height: 40px;
     $toast-bg: rgba(0,0,0,0.75);
-    .toast{
-        font-size: $font-size;height: $toast-height;line-height: 1.8;position: fixed;
+    .toast{font-size: $font-size;height: $toast-height;line-height: 1.8;position: fixed;
         top: 0;left: 50%;transform: translateX(-50%);
         display:flex;align-items: center;background: $toast-bg;
         border-radius: 4px;box-shadow: 0 0 3px 0 rgba(0,0,0,0.50);
         color: #eeeeee;padding: 0 16px;
     }
+    .close{padding-left: 16px;cursor: pointer;}
+    .line{height: 80%;border-left: 1px solid #666;margin-left: 16px;}
 </style>
