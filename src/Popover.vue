584bdfbd3ad44f92df2e0dedd22a3edf6d6de817
wuxiao <1291383707@qq.com>
2019-11-16 20:36:02 +0800

    实现简单的 popover

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="popover" @click.stop="xxx">
-        <div class="content-wrapper" v-if="visible" @click.stop>
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
+
     </div>
 
 </template>
@@ -14,11 +17,18 @@
         data(){
             return {visible: false}
         },
+        mounted:{
+
+        },
         methods:{
             xxx(){
                 this.visible = !this.visible
                 if (this.visible===true) {
                     this.$nextTick(()=>{
+                        document.body.appendChild(this.$refs.contentWrapper)
+                        let {width,height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                        this.$refs.contentWrapper.style.left = left + 'px'
+                        this.$refs.contentWrapper.style.top = top + 'px'
                         let eventHandle = ()=>{
                             this.visible = false
                             document.removeEventListener('click', eventHandle)
@@ -29,7 +39,8 @@
 
                 }
             }
-        }
+        },
+
 
     }
 </script>
@@ -42,9 +53,8 @@
     }
     .content-wrapper{
         position: absolute;
-        bottom: 100%;
-        left: 0;
         border: 1px solid red;
         box-shadow: 0 0 3px rgba(0,0,0,0.5);
+        transform: translateY(-100%);
     }
 </style>
