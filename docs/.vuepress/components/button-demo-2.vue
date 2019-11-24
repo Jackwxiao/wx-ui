78e22cc22377633640041047bfe7987847ae37ef
wuxiao <1291383707@qq.com>
2019-11-24 13:15:25 +0800

    添加 组合按钮 文档 icon

--- docs/.vuepress/components/button-demo-2.vue
+++ docs/.vuepress/components/button-demo-2.vue
@@ -3,9 +3,9 @@
         <h2>组合按钮用法</h2>
         <p><strong>预览</strong></p>
         <g-button-group>
-            <g-button>上一页</g-button>
+            <g-button icon="left">上一页</g-button>
             <g-button>...</g-button>
-            <g-button>下一页</g-button>
+            <g-button icon="right" icon-position="right">下一页</g-button>
         </g-button-group>
         <p><strong>代码</strong></p>
         <pre><code>{{content}}</code></pre>
@@ -19,9 +19,9 @@
         data () {
             return {
                 content:`<g-button-group>
-            <g-button>上一页</g-button>
+            <g-button icon="left">上一页</g-button>
             <g-button>...</g-button>
-            <g-button>下一页</g-button>
+            <g-button icon="right" icon-position="right">下一页</g-button>
 </g-button-group>
       `.replace(/^ {8}/gm, '').trim()
             }
