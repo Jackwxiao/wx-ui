9455f6b5be54e0f1148e1bbca78561beae923b90
wuxiao <1291383707@qq.com>
2019-11-24 12:33:08 +0800

    解决 icon 不出来的Bug

--- src/icon.vue
+++ src/icon.vue
@@ -1,6 +1,6 @@
 <template>
     <svg  class="g-icon">
-        <use :xlink:href=`#i-${name}`></use>
+        <use :xlink:href="`#i-${name}`"></use>
     </svg>
 </template>
 <script>
@@ -8,9 +8,9 @@
 
     export default {
         name: 'GuLuIcon',
-        props:['name']
+        props: ['name']
 }
 </script>
 <style lang="scss" scoped>
-    .g-icon {width: 1em;height: 1em;}
+    .g-icon {width: 1em; height: 1em; }
 </style>
